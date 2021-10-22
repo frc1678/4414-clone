b488db288bf19440b2e9646417f70953e9888914
oliviasagayaradj <55220280+oliviasagayaradj@users.noreply.github.com>
2021-10-21 20:06:42 -0700

    address comments

--- src/main/java/com/team1678/frc2021/subsystems/Hood.java
+++ src/main/java/com/team1678/frc2021/subsystems/Hood.java
@@ -1,29 +1,32 @@
 
 package com.team1678.frc2021.subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Servo;
 
 import com.team1678.frc2021.Constants;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.AnalogEncoder;
 
 public class Hood extends LinearServo {
     private static Hood mInstance;
+    private AnalogEncoder mEncoder;
     private boolean mHoming = true;
     private Servo hoodServoA;
     private Servo hoodServoB;
 
     //Create new Hood subsystem
-    public Hood() {
+    private Hood(final ServoSubsystem.ServoSubsystemConstants constants) {
         super(mInstance.getChannel(), mInstance.hashCode(), mInstance.getRaw());
-        super.getChannel();
-        hoodServoA = new Servo(Constants.kHoodServoA);
-        hoodServoB = new Servo(Constants.kHoodServoB);
+        hoodServoA = new Servo(Constants.kHoodConstants.kHoodServoA);
+        hoodServoB = new Servo(Constants.kHoodConstants.kHoodServoB);
     }
 
     public void hoodExtend() {
-        hoodServoA.set(Constants.kHoodSpeed);
-        hoodServoB.set(Constants.kHoodSpeed);
+        hoodServoA.set(Constants.kHoodConstants.kHoodSpeed);
+        hoodServoB.set(Constants.kHoodConstants.kHoodSpeed);
     }
 
     public void hoodStop() {
@@ -55,9 +58,11 @@ public class Hood extends LinearServo {
     public synchronized void readPeriodicInputs() {
         if (mHoming && atHomingLocation()) {
             System.out.println("is homing");
+            mEncoder.reset();
             System.out.println("Homed!!!");
             mHoming = false;
         }
+        SmartDashboard.putNumber(("Hood Encoder Readout"), mEncoder.getDistance());
         super.readPeriodicInputs();
     }
 
