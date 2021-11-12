b64f4942ea9d0dde3624292cd69b017fb5ebec51
oliviasagayaradj <55220280+oliviasagayaradj@users.noreply.github.com>
2021-11-11 19:06:33 -0800

    Update DropIntakeCommand.java

--- src/main/java/com/team1678/frc2021/commands/DropIntakeCommand.java
+++ src/main/java/com/team1678/frc2021/commands/DropIntakeCommand.java
@@ -1,22 +1,34 @@
 package com.team1678.frc2021.commands;
 
 import com.team1678.frc2021.subsystems.Turret;
-import com.team1678.frc2021.subsystem.Superstructure;
+import com.team1678.frc2021.subsystems.Superstructure;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DropIntakeCommand extends CommandBase {
-    private final mTurret = turret; 
-    private final mSuperstructure = superstructure;
-}
+public class DroppingIntakeCommand extends CommandBase {
+    
+    private final Superstructure mSuperstructure;
+    private final double mTurretAngle;
+    private double mTurretSetpoint = 0.0; 
 
-@Override
-public void initialize() {
-    //set turret to 90 degrees 
-    private double mTurretSetpoint = -90.0;
-}
+    public DroppingIntakeCommand(Superstructure superstructure, double turretAngle) {
+        mSuperstructure = superstructure;
+        mTurretAngle = turretAngle;
+    }
 
-@Override
-public boolean execute() {
-    mSuperstructure.enableTurret(true);
+    @Override
+    public void initialize() {
+        mSuperstructure.setWantDroppingIntake(Rotation2d.fromDegrees(mTurretAngle));
+
+    }
+
+    @Override
+    public void execute() {
+        mSuperstructure.setWantDroppingIntake(Rotation2d.fromDegrees(mTurretAngle));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return mSuperstructure.isDroppingIntake && Math.abs(mSuperstructure.mTurret.getAngle.getDegrees() - 90);
+    }
 }
